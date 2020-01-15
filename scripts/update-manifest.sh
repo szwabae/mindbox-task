f43e02143ea201cb50eac0adf21a111552c9d570
Ewa Szwaba <ewaszwaba@Ewas-MacBook-Air.local>
2020-01-15 19:25:10 +0100

    Remove manifests and update script

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -1,24 +1,31 @@
 #!bin/sh
 
 TAGS=".tags"
-DEPLOYMENT="mindbox-task-dev.yaml"
+DEPLOYMENT="prod/mindbox-task-prod.yaml"
 
 if ![ -f $TAGS ]; then
 	echo "$TAGS not found."
 	exit 1;
 fi
 
+TAG=$(cat .tags)
+
+cd ..
+
+apk add git
+git clone https://github.com/szwabae/mindbox-k8s.git
+
+cd mindbox-k8s
+
 if ![ -f $DEPLOYMENT ]; then
 	echo "$DEPLOYMENT not found."
 	exit 1;
 fi
 
-TAG=$(cat .tags)
-
 sed -i "s|\(image: szwabaewa/mindbox-task\):.*$|\1:$TAG|" $DEPLOYMENT
-apk add git
+
 git add $DEPLOYMENT
-git commit -m "[DRONE.io] k8s update deployment [ci skip]"
-git remote add authenticated-origin https://szwabae:${GITHUB_API_KEY}@github.com/szwabae/mindbox-task
+git commit -m "[DRONE.io] k8s update deployment"
+git remote add authenticated-origin https://szwabae:${GITHUB_API_KEY}@github.com/szwabae/mindbox-k8s
 git fetch authenticated-origin
 git push authenticated-origin master
