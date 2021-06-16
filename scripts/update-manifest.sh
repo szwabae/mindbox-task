42569981495a448979ccf9da79432fdb237ade98
Ewa Szwaba <ewa.szwaba@mindboxgroup.com>
2021-06-16 10:17:04 +0200

    Add origin

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -26,6 +26,6 @@ sed -i "s|\(image: szwabaewa/mindbox-task\):.*$|\1:$TAG|" $DEPLOYMENT
 
 git add $DEPLOYMENT
 git commit -m "[DRONE.io] k8s update deployment"
-git remote add authenticated-origin https://szwabae:${GITHUB_API_KEY}@github.com/szwabae/mindbox-k8s
-git fetch authenticated-origin
-git push authenticated-origin master
+git remote origin https://szwabae:${GITHUB_API_KEY}@github.com/szwabae/mindbox-k8s
+git fetch origin
+git push origin master
